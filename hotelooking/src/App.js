f1af3b20c47c0b079559b0b965a0d692b4130996
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-05-12 12:58:19 +0200

    created the pages and routes

--- hotelooking/src/App.js
+++ hotelooking/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import HotelList from "./pages/hotelList/HotelList";
+import SingleHotel from "./pages/hotel/SingleHotel";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<HotelList />} />
+        <Route path="/hotels/:id" element={<SingleHotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

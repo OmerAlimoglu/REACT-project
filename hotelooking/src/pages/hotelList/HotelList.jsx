8df06271e136fab8da2fe3bb4e31f637cd3c9f11
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-05-13 15:39:17 +0200

    finished the search bar, related component and styling

--- hotelooking/src/pages/hotelList/HotelList.jsx
+++ hotelooking/src/pages/hotelList/HotelList.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 
 const HotelList = () => {
-  return <div>hotel</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="hotelList" />
+    </div>
+  );
 };
 
 export default HotelList;

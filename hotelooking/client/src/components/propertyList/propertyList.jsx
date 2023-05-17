1e38192548304cadb25af39a9de0844f9601a02e
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-05-17 14:32:53 +0200

    finished the home page

--- hotelooking/client/src/components/propertyList/propertyList.jsx
+++ hotelooking/client/src/components/propertyList/propertyList.jsx
@@ -1,63 +1,40 @@
+import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
 const PropertyList = () => {
+  const { data, loading, error } = useFetch("/hotels/countByType");
+  const images = [
+    "https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o=",
+    "https://r-xx.bstatic.com/xdata/images/hotel/263x210/119467716.jpeg?k=f3c2c6271ab71513e044e48dfde378fcd6bb80cb893e39b9b78b33a60c0131c9&o=",
+    "https://r-xx.bstatic.com/xdata/images/xphoto/263x210/45450064.jpeg?k=4d4ea22dc4828fd55a3889e90531c9841ddb2d9abf460c420cdd24f2a9b658d2&o=",
+    "https://r-xx.bstatic.com/xdata/images/hotel/263x210/100235855.jpeg?k=5b6e6cff16cfd290e953768d63ee15f633b56348238a705c45759aa3a81ba82b&o=",
+    "https://q-xx.bstatic.com/xdata/images/xphoto/263x210/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o=",
+  ];
+
   return (
     <div className="pList">
-      <div className="pListItem">
-        <img
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&o="
-          alt="hotel Room"
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://r-xx.bstatic.com/xdata/images/hotel/263x210/119467716.jpeg?k=f3c2c6271ab71513e044e48dfde378fcd6bb80cb893e39b9b78b33a60c0131c9&o="
-          alt="kitchen"
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Apartments</h1>
-          <h2>130 apartments</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://r-xx.bstatic.com/xdata/images/xphoto/263x210/45450064.jpeg?k=4d4ea22dc4828fd55a3889e90531c9841ddb2d9abf460c420cdd24f2a9b658d2&o="
-          alt="riad"
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Riads</h1>
-          <h2>72 riads</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://r-xx.bstatic.com/xdata/images/hotel/263x210/100235855.jpeg?k=5b6e6cff16cfd290e953768d63ee15f633b56348238a705c45759aa3a81ba82b&o="
-          alt="villa"
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Villas</h1>
-          <h2>102 villas</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img
-          src="https://q-xx.bstatic.com/xdata/images/xphoto/263x210/45450074.jpeg?k=7039b03a94f3b99262c4b3054b0edcbbb91e9dade85b6efc880d45288a06c126&o="
-          alt="cottages"
-          className="pListImg"
-        />
-        <div className="pListTitles">
-          <h1>Cottages</h1>
-          <h2>82 cottages</h2>
-        </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((item, index) => (
+              <div className="pListItem" key={index}>
+                <img
+                  src={images[index]}
+                  alt="hotel Room"
+                  className="pListImg"
+                />
+                <div className="pListTitles">
+                  <h1>{item.type}</h1>
+                  <h2>
+                    {item.count} {item.type}
+                  </h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
   );
 };
